15a74c767cf7aeb4db6c72d6e4674b43df285b28
mohamed <mdiaa404@gmail.com>
2021-10-21 16:00:39 +0200

    update model url

--- models.py
+++ models.py
@@ -7,11 +7,10 @@ import psycopg2
 # heroku database
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-'''
+
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-'''
 # # local
 # dotenv_path = join(dirname(__file__), '.env')
 # load_dotenv(dotenv_path)
